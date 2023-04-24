59b8b991b872264979dcd984a744486a5f0f4248
Deep <55054090+deepend-dev@users.noreply.github.com>
2023-04-24 13:02:10 +0530

    fix: add karpenter

--- v4.24.0/fully-private-eks-cluster/eks/main.tf
+++ v4.24.0/fully-private-eks-cluster/eks/main.tf
@@ -165,17 +165,32 @@ module "kubernetes_addons" {
   enable_amazon_prometheus      = try(var.enable_addons.amazon_prometheus, false)
 }
 
-# data "kubectl_path_documents" "karpenter_provisioners" {
-#   pattern = "${path.module}/kubernetes/karpenter/*"
-#   vars = {
-#     azs                     = join(",", local.azs)
-#     iam-instance-profile-id = "${local.name}-${local.node_group_name}"
-#     eks-cluster-id          = local.name
-#     eks-vpc_name            = local.name
-#   }
-# }
 
-# resource "kubectl_manifest" "karpenter_provisioner" {
-#   for_each  = toset(data.kubectl_path_documents.karpenter_provisioners.documents)
-#   yaml_body = each.value
-# }
+data "kubectl_path_documents" "karpenter_provisioners" {
+
+  depends_on = [
+    module.kubernetes_addons
+  ]
+
+  count = try(var.enable_addons.karpenter, true) ? 1 : 0
+
+  pattern = "${path.module}/kubernetes/karpenter/*"
+  vars = {
+    azs                     = join(",", local.azs)
+    iam-instance-profile-id = "${local.name}-${local.node_group_name}"
+    eks-cluster-id          = local.name
+    eks-vpc_name            = local.name
+  }
+}
+
+resource "kubectl_manifest" "karpenter_provisioner" {
+
+  depends_on = [
+    module.kubernetes_addons
+  ]
+
+  count = try(var.enable_addons.karpenter, true) ? 1 : 0
+
+  for_each  = toset(data.kubectl_path_documents.karpenter_provisioners.documents)
+  yaml_body = each.value
+}
