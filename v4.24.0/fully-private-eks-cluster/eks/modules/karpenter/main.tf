15d1e0e18f92cf7c37763d3edb52fb7ea20d3e78
Deep <55054090+deepend-dev@users.noreply.github.com>
2023-04-24 15:31:10 +0530

    update: karpenter updagte

--- v4.24.0/fully-private-eks-cluster/eks/modules/karpenter/main.tf
+++ v4.24.0/fully-private-eks-cluster/eks/modules/karpenter/main.tf
@@ -1,3 +1,11 @@
+locals {
+  azs = slice(data.aws_availability_zones.available.names, 0, 3)
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 data "kubectl_path_documents" "karpenter_provisioners" {
 
   pattern = var.pattern == "null" ? "${path.module}/../../kubernetes/karpenter/*" : var.pattern
@@ -10,4 +18,27 @@ data "kubectl_path_documents" "karpenter_provisioners" {
 resource "kubectl_manifest" "karpenter_provisioner" {
   for_each  = toset(data.kubectl_path_documents.karpenter_provisioners.documents)
   yaml_body = each.value
+}
+
+terraform {
+  required_version = ">= 1.0.1"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.72"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.10"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.4.1"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14"
+    }
+  }
 }
